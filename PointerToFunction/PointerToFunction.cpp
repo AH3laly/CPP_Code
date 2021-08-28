1969821336e306407578659b7d72f64979bb95ef
AH3laly <AH3laly@gmail.com>
2021-08-28 09:35:37 +0200

    Update PointerToFunction

--- PointerToFunction/PointerToFunction.cpp
+++ PointerToFunction/PointerToFunction.cpp
@@ -12,7 +12,7 @@ void printStudentInfo(Student *studentToPrint, void(*pf)(Student)){
     pf(*studentToPrint);
 }
 
-void print(Student s){
+void print_handler(Student s){
     cout << "Welcome, The information of the sent object is " << endl;
     cout << s.id << " : " << s.name << endl;
 }
@@ -23,12 +23,12 @@ int main(){
     s.id = 1;
     s.name = "Abdo";
 
-    printStudentInfo(&s, &print);
+    printStudentInfo(&s, &print_handler);
     
     // OR:
-    // Create a pointer to the print function and pass it to printStudentInfo
-    //void (*print1) (Student) = print;
-    //printStudentInfo(&s, print1);
+    // Create a pointer to the print_handler function and pass it to printStudentInfo
+    //void (*print_handler_ptr) (Student) = print_handler;
+    //printStudentInfo(&s, print_handler_ptr);
     
     return 0;
 }
